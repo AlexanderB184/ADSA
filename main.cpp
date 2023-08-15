49c94075b95bc98072143c08c175f80e306fc857
AlexanderB184 <a1849988@adelaide.edu.au>
2023-08-15 13:07:41 +0930

    Multiplication working

--- main.cpp
+++ main.cpp
@@ -2,39 +2,30 @@
 #include <iostream>
 #include <string>
 
-using std::cout;
-using std::endl;
 using std::string;
 
-string& reverse(string& A) {
-  int n = A.length() - 1;
-  for (int i = 0; i < A.length() / 2; i++) {
+string reverse(string A) {
+  size_t n = A.length() - 1;
+  size_t m = A.length() / 2;
+  for (size_t i = 0; i < m; i++) {
     std::swap(A[i], A[n - i]);
   }
   return A;
 }
 
-void leftpad(string& A, int n) {
+void leftpad(string& A, size_t n) {
   A.resize(A.length() + n);
-  for (int i = 0; i < A.length(); i++) {
+  for (long i = A.length() - 1; i >= 0; i--) {
     A[i + n] = A[i];
   }
-  for (int i = 0; i < n; i++) {
+  for (size_t i = 0; i < n; i++) {
     A[i] = '0';
   }
 }
 
-void make_same_length(string& A, string& B) {
-  if (A.length() < B.length()) {
-    leftpad(A, B.length() - A.length());
-  } else if (A.length() > B.length()) {
-    leftpad(B, A.length() - B.length());
-  }
-}
-
 string& strip_trailing_zeros(string& A) {
-  int i = A.length();
-  while (--i >= 0 && A[i] == '0') {
+  size_t i = A.length();
+  while (--i > 0 && A[i] == '0') {
     A.pop_back();
   }
   return A;
@@ -43,8 +34,8 @@ string& strip_trailing_zeros(string& A) {
 string school_addition(const string& lhs, const string& rhs, int base) {
   string res;
   int carry = 0;
-  int i = 0;
-  int n = std::max(lhs.length(), rhs.length());
+  size_t i = 0;
+  size_t n = std::max(lhs.length(), rhs.length());
   while (i < n) {
     int a = 0, b = 0;
     if (i < lhs.length()) a = lhs[i] - '0';
@@ -62,8 +53,8 @@ string school_addition(const string& lhs, const string& rhs, int base) {
 string school_subtraction(const string& lhs, const string& rhs, int base) {
   string res;
   int borrow = 0;
-  int i = 0;
-  int n = std::max(lhs.length(), rhs.length());
+  size_t i = 0;
+  size_t n = std::max(lhs.length(), rhs.length());
   while (i < n) {
     int a = 0, b = 0;
     if (i < lhs.length()) a = lhs[i] - '0';
@@ -86,13 +77,13 @@ string school_subtraction(const string& lhs, const string& rhs, int base) {
 string school_multiplication(const string& lhs, const string& rhs, int base) {
   assert(rhs.length() < 2);
   if (rhs.length() == 0) {
-    return "";
+    return "0";
   }
   string res;
   res.reserve(lhs.length() + 1);
   char carry = '0';
   int m = (rhs[0] - '0');
-  int i = 0;
+  size_t i = 0;
   while (i < lhs.length()) {
     int x = (lhs[i] - '0') * m + (carry - '0');
     carry = (x / base) + '0';
@@ -111,8 +102,8 @@ string karatsuba_multiplication(const string& lhs, const string& rhs,
   } else if (rhs.length() < 2) {
     return school_multiplication(lhs, rhs, base);
   }
-  int m = std::max(lhs.length(), rhs.length());
-  int m2 = m - m / 2;
+  size_t m = std::max(lhs.length(), rhs.length());
+  size_t m2 = m - m / 2;
   string low1 = lhs.substr(0, m2);
   string high1 = lhs.substr(m2);
   string low2 = rhs.substr(0, m2);
@@ -122,30 +113,23 @@ string karatsuba_multiplication(const string& lhs, const string& rhs,
       karatsuba_multiplication(school_addition(low1, high1, base),
                                school_addition(low2, high2, base), base);
   string z2 = karatsuba_multiplication(high1, high2, base);
-  string z3 = school_addition(z2, z0, base);
-  string z4, z5;
-  if (stoi(z3) > stoi(z1)) {
-    cout << "a" << endl;
-    z4 = school_subtraction(z3, z1, base);
-    leftpad(z4, m2);
-    leftpad(z2, m2 * 2);
-    z5 = school_subtraction(z2, z4, base);
-    return school_addition(z5, z0, base);
-  } else {
-    cout << "b" << endl;
-    z4 = school_subtraction(z1, z3, base);
-    leftpad(z4, m2);
-    leftpad(z2, m2 * 2);
-    z5 = school_addition(z2, z4, base);
-    return school_addition(z5, z0, base);
-  }
+  string z4 = school_subtraction(school_subtraction(z1, z2, base), z0, base);
+  leftpad(z4, m2);
+  leftpad(z2, m2 * 2);
+  return school_addition(z0, school_addition(z2, z4, base), base);
+}
+
+string add(const string& A, const string& B, const string& base) {
+  return reverse(school_addition(reverse(A), reverse(B), stoi(base)));
+}
+
+string mult(const string& A, const string& B, const string& base) {
+  return reverse(karatsuba_multiplication(reverse(A), reverse(B), stoi(base)));
 }
 
 int main(int argc, char const* argv[]) {
-  string a = "999";
-  string b = "1000";
-  string base = "10";
-  string res = school_subtraction(reverse(a), reverse(b), stoi(base));
-  cout << reverse(res) << endl;
+  assert(argc == 4);
+  std::cout << add(argv[1], argv[2], argv[3]) << " "
+            << mult(argv[1], argv[2], argv[3]) << " 0" << std::endl;
   return 0;
 }
